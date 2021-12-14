f6c141e8537a71a530dd900f2f2bdd63e55dd8bc
MariaRosenholm <maria.rosenholm@gmail.com>
2021-12-14 12:01:12 +0200

    changed the whole card clickable

--- src/components/Animals/Animalcard.js
+++ src/components/Animals/Animalcard.js
@@ -5,19 +5,19 @@ const Animalcard = (props) => {
   const match = useRouteMatch();
 
   return (
-    <div className="card">
-      <h2>
-        {props.name.substring(0, 1).toUpperCase() +
-          props.name.substring(1, props.name.length)}
-      </h2>
-      <img
-        alt="animal"
-        src={`https://source.unsplash.com/1600x900/?${props.name}`}
-      />
-      <Link className="links linkListItem" to={`${match.url}/${props.name}`}>
+    <Link className="links linkListItem" to={`${match.url}/${props.name}`}>
+      <div className="card">
+        <h2>
+          {props.name.substring(0, 1).toUpperCase() +
+            props.name.substring(1, props.name.length)}
+        </h2>
+        <img
+          alt="animal"
+          src={`https://source.unsplash.com/1600x900/?${props.name}`}
+        />
         Read more
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
